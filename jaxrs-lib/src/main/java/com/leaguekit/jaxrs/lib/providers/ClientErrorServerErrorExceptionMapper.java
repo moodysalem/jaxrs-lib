89a98f717346a4357de90e169145cd8453f1b3b7
Moody Salem <moody.salem@gmail.com>
2015-10-03 15:56:51 -0500

    Allow redirection exceptions to pass through

--- jaxrs-lib/src/main/java/com/leaguekit/jaxrs/lib/providers/ClientErrorServerErrorExceptionMapper.java
+++ jaxrs-lib/src/main/java/com/leaguekit/jaxrs/lib/providers/ClientErrorServerErrorExceptionMapper.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.RedirectionException;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+public class ClientErrorServerErrorExceptionMapper implements ExceptionMapper<WebApplicationException> {
 
     private static final ObjectMapper om = new ObjectMapper();
 
-    @Override
-    public Response toResponse(WebApplicationException e) {
+    private Response mapException(WebApplicationException e) {
         ArrayNode errors = om.createArrayNode();
 
         ObjectNode errObj = om.createObjectNode();
@@ -26,5 +28,13 @@ public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplica
             .entity(errors)
             .header(RequestProcessingExceptionMapper.NUMBER_OF_ERRORS_HEADER, errors.size())
             .build();
+    }
+
+    @Override
+    public Response toResponse(WebApplicationException e) {
+        if (e instanceof RedirectionException) {
+            return e.getResponse();
+        }
+        return mapException(e);
     }
 }
