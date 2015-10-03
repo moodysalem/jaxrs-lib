f0ccd4a0be1a0e7c675e2127a37a3e9efedb9086
Moody Salem <moody.salem@gmail.com>
2015-10-03 14:07:37 -0500

    Add a template engine

--- jaxrs-lib/src/main/java/com/leaguekit/jaxrs/lib/BaseApplication.java
+++ jaxrs-lib/src/main/java/com/leaguekit/jaxrs/lib/BaseApplication.java
@@ -1,6 +1,5 @@
 package com.leaguekit.jaxrs.lib;
 
-import com.leaguekit.jaxrs.lib.exceptions.RequestProcessingException;
 import com.leaguekit.jaxrs.lib.filters.CORSFilter;
 import com.leaguekit.jaxrs.lib.filters.HTTPSFilter;
 import com.leaguekit.jaxrs.lib.providers.RequestProcessingExceptionMapper;
@@ -9,11 +8,13 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.EncodingFilter;
+import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 
 public class BaseApplication extends ResourceConfig {
     public BaseApplication() {
         // register the things that are typically used by a JAX-RS application
         register(JacksonFeature.class);
+        register(FreemarkerMvcFeature.class);
 
         register(CORSFilter.class);
         register(HTTPSFilter.class);
