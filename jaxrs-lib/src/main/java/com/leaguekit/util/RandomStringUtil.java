7f1913a1ad0a4e5c2d6be823c294665cbbf6d170
Moody Salem <moody.salem@gmail.com>
2015-10-03 23:58:14 -0500

    Random string utility

---
+++ jaxrs-lib/src/main/java/com/leaguekit/util/RandomStringUtil.java
@@ -0,0 +1,40 @@
+package com.leaguekit.util;
+
+import java.security.SecureRandom;
+
+public class RandomStringUtil {
+
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+
+    private static final char[] SYMBOLS;
+    private static final int NUM_SYMBOLS;
+
+    static {
+        StringBuilder tmp = new StringBuilder();
+        for (char ch = '0'; ch <= '9'; ++ch) {
+            tmp.append(ch);
+        }
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            tmp.append(ch);
+        }
+        for (char ch = 'A'; ch <= 'Z'; ++ch) {
+            tmp.append(ch);
+        }
+        SYMBOLS = tmp.toString().toCharArray();
+        NUM_SYMBOLS = SYMBOLS.length;
+    }
+
+    public static String randomAlphaNumeric(int length) {
+        StringBuilder sb = new StringBuilder();
+        if (length < 0) {
+            throw new IllegalArgumentException();
+        }
+        while (length > 0) {
+            length --;
+            sb.append(SYMBOLS[SECURE_RANDOM.nextInt(NUM_SYMBOLS)]);
+        }
+        return sb.toString();
+    }
+
+}
